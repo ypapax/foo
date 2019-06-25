ef9871ff5e60dbf965469d88cbcc1e8af5dde42b
Maxim Yefremov <maxYefr@gmail.com>
2019-06-25 08:21:16 +0300

    go colorable works

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/ypapax/foo
 
 go 1.12
+
+require (
+	github.com/mattn/go-colorable v0.1.2
+	github.com/sirupsen/logrus v1.4.2
+	github.com/ypapax/stack v0.0.0-20190623065105-9445e0f9ee16
+)
--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "github.com/ypapax/stack"
+import (
+	colorable "github.com/mattn/go-colorable"
+	"github.com/sirupsen/logrus"
+)
 
 func main() {
-	stack.Get()
+	logrus.SetFormatter(&logrus.TextFormatter{ForceColors: true})
+	logrus.SetOutput(colorable.NewColorableStdout())
+
+	logrus.Info("succeeded")
+	logrus.Warn("not correct")
+	logrus.Error("something error")
+	logrus.Fatal("panic")
 }
